825e79fe10ca526a20e7da4a66d68da6bbcbc1d7
Marcoamigue <marcoamigue@gmail.com>
2026-01-30 11:35:55 -0600

    Practica-Modulo-5
    Practica-Modulo-5

--- Carpeta proyecto unity/My project (1)/Assets/Scrips/HolaMundo.cs
+++ Carpeta proyecto unity/My project (1)/Assets/Scrips/HolaMundo.cs
@@ -2,11 +2,15 @@ using UnityEngine;
 
 public class HolaMundo : MonoBehaviour
 {
+
+
+    int x;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        x = 0;
         print("algo paso");
-        Debug.Log("Algo paso");
+
         Debug.LogWarning("Algo salio medianamente mal");
         Debug.LogError("Algo salio muy mal");
     }
@@ -14,6 +18,25 @@ public class HolaMundo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //x = x + 1;
+        //Debug.Log("x");
+
+        Debug.Log("Hola Desde Update");
+    }
+    private void FixedUpdate()
+    {
+        Debug.LogWarning("Hola desde Fixed Update cada 50 frames");
+    }
+    private void LateUpdate()
+    {
+        Debug.Log("Hola desde Late Update");
+    }
+    private void OnEnable()
+    {
+        Debug.LogWarning("El objeto ha sido habilitado");   
+    }
+    private void OnDisable()
+    {
+        Debug.LogWarning("El objeto ha sido deshabilitado");
     }
 }
